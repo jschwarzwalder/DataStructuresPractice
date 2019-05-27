038bc175ed31ff0dfe8aec4ab47e0913c2663cc8
Jami Schwarzwalder <jamisch@amazon.com>
2019-05-26 17:08:49 -0700

    New data structure

---
+++ src/Trie.java
@@ -0,0 +1,19 @@
+public class Trie {
+    public static class Node {
+        private static int NUMBER_OF_CHARACTHERS = 26;
+        Node[] children = new Node[NUMBER_OF_CHARACTHERS];
+
+        private Node getNode(char c) {}
+
+        private void setNode (char c, Node node){}
+
+        private void add (String s, int index){}
+
+        public int findCount(String s, int index) {}
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
